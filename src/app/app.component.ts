328c1f559ccda76167d84562c337138893d5a247
Johan Persson <johanopersson@gmail.com>
2020-10-09 15:34:24 +0200

    Added repro case for image import + file name transform + snapshot.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import imageSrc from "../assets/photo.jpg";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'angular-builders-jest-default-config-repro';
+  title = "angular-builders-jest-default-config-repro";
+  imageSrc = imageSrc;
 }
